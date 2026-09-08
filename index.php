b84361ef2708c7d63b2cd6cd3e39550f4d69c352
ffogells <samllg+github@proton.me>
2026-09-08 09:49:00 +0200

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sam's PHP</title>
+</head>
+<body>
+    <h1>Welkom</h1>
+    <h2>Les 1</h2>
+    <a href="Les-1/Opdracht-1.php">Opdracht 1</a><br>
+    <a href="Les-1/Opdracht-2.php">Opdracht 2</a><br>
+    <a href="Les-1/Opdracht-3.php">Opdracht 3</a><br>
+    <a href="Les-1/Opdracht-4.php">Opdracht 4</a><br>
+    <a href="Les-1/Opdracht-5.php">Opdracht 5</a><br>
+    <a href="Les-1/Opdracht-6.php">Opdracht 6</a><br>
+</body>
+</html>
