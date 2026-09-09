d280ef202f488502713f722b4ef6b8714a122e72
ffogells <samllg+github@proton.me>
2026-09-09 10:11:07 +0200

    Opdracht 9 en fix voor opdracht 6

---
+++ Les-2/Opdracht-9/formulier.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simpel formulier</title>
+</head>
+<body>
+    <form method="POST" action="resultaat.php">
+        Voornaam: <input type="text" name="voornaam"><br>
+        Achternaam: <input type="text" name="achternaam"><br>
+        Geboortejaar: <input type="text" name="geboortejaar"><br>
+        <input type="submit" value="Verstuur">
+    </form>
+</body>
+</html>
--- index.php
+++ index.php
@@ -15,6 +15,7 @@
     <a href="Les-1/Opdracht-6.php">Opdracht 6</a><br>
     <h2>Les 2</h2>
     <a href="Les-2/Opdracht-8/formulier.php">Opdracht 8</a><br>
+    <a href="Les-2/Opdracht-9/formulier.php">Opdracht 9</a><br>
     <h2>Overig</h2>
     <a href="LICENSE.md">Licentie</a><br>    
 </body>
